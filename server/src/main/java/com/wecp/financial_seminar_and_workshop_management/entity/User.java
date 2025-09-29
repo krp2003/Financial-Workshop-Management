b79b2dc06345185dbd8cea889597b221bbe31a68
username <wecp-dummy@gmail.com>
2025-09-29 04:36:06 +0000

    Update User

--- server/src/main/java/com/wecp/financial_seminar_and_workshop_management/entity/User.java
+++ server/src/main/java/com/wecp/financial_seminar_and_workshop_management/entity/User.java
@@ -1,7 +1,5 @@
 package com.wecp.financial_seminar_and_workshop_management.entity;
 
-
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -13,5 +11,100 @@ import java.util.Set;
 
 @Table(name = "users") // do not change table name
 public class User {
-    // implement user entity here
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String role; // INSTITUTION, PROFESSIONAL, PARTICIPANT
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<Enrollment> enrollments = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Set<Feedback> feedbacks = new HashSet<>();
+
+    @ManyToMany(mappedBy = "professionals")
+    private List<Event> events = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, String role, Set<Enrollment> enrollments,
+            Set<Feedback> feedbacks, List<Event> events) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.enrollments = enrollments;
+        this.feedbacks = feedbacks;
+        this.events = events;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Set<Enrollment> getEnrollments() {
+        return enrollments;
+    }
+
+    public void setEnrollments(Set<Enrollment> enrollments) {
+        this.enrollments = enrollments;
+    }
+
+    public Set<Feedback> getFeedbacks() {
+        return feedbacks;
+    }
+
+    public void setFeedbacks(Set<Feedback> feedbacks) {
+        this.feedbacks = feedbacks;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+
 }
